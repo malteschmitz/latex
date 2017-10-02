0b6f1b9e78feedf6d93883439a7d3327050efb5e
Malte Schmitz <m@mlte.de>
2017-10-02 13:10:15 +0200

    Add old 2016 website

---
+++ web/2016/js/latex.js
@@ -0,0 +1,33 @@
+$(function() {
+  sh_highlightDocument();
+
+  $('#toc').tableOfContents($('#main'), {depth: 2});
+  $('#toc ul').addClass('nav');
+
+  var win = $(window);
+  var bd = $(document.body);
+
+  bd.scrollspy({
+    target: '.latex-sidebar',
+  });
+
+  win.on('load', function () {
+    bd.scrollspy('refresh');
+  });
+
+  window.setTimeout(function () {
+    var sidebar = $('.latex-sidebar');
+    sidebar.affix({
+      offset: {
+        top: function () {
+          var offsetTop = sidebar.offset().top;
+          var sideBarMargin = parseInt(sidebar.children(0).css('margin-top'), 10);
+          return (this.top = offsetTop - sideBarMargin);
+        },
+        bottom: function () {
+          return (this.bottom = $('.latex-footer').outerHeight(true));
+        }
+      }
+    });
+  }, 100);
+});
